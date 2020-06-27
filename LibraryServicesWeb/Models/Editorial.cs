29ded44074ff1d2a4afa3ecf9b95aa33770a5880
Steven112 <54567861+Steven112@users.noreply.github.com>
2020-06-27 17:40:28 -0400

    Modificacion 1

--- LibraryServicesWeb/Models/Editorial.cs
+++ LibraryServicesWeb/Models/Editorial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,26 @@ namespace LibraryServicesWeb.Models
 {
     public class Editorial
     {
+        [Key]
+        public int EditorialId { get; set; }
+        public string Nombre { get; set; }
+        public string Dirrecion { get; set; }
+        public int UsuarioId { get; set; }
+
+        public Editorial()
+        {
+            EditorialId = 0;
+            Nombre = string.Empty;
+            Dirrecion = string.Empty;
+            UsuarioId = 0;
+        }
+
+        public Editorial(int editorialId, string nombre, string dirrecion, int usuarioId)
+        {
+            EditorialId = editorialId;
+            Nombre = nombre;
+            Dirrecion = dirrecion;
+            UsuarioId = usuarioId;
+        }
     }
 }
